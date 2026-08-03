abeb8764a58f663bf73d61a10f207e0c9bd21f1f
Yuyang Liu <yuyangliu.0728@gmail.com>
2026-08-03 15:47:35 -0400

    Update integration_distill.sh

--- test/integration_distill.sh
+++ test/integration_distill.sh
@@ -20,7 +20,8 @@ YAML
 
 bundle exec jekyll build --config "_config.yml,${tmp_override}" -d "${tmp_site}" >/dev/null
 
-distill_page="${tmp_site}/blog/2021/distill/index.html"
+# distill_page="${tmp_site}/blog/2021/distill/index.html"
+distill_page="${tmp_site}/blog/2018/distill/index.html"
 
 if [ ! -f "${distill_page}" ]; then
   echo "distill page was not generated at ${distill_page}" >&2
